e6e731a23fd60e17d49c4736ee2fe2b644ce9c70
priya940 <pv871515@gmail.com>
2024-02-20 21:33:24 +0530

    file

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='h-20 flex items-center w-full text-black'>
+    <div className='h-20 flex items-center w-full text-white'>
         <div className='text-3xl pl-20 font-bold'>Searchify</div> 
          </div>
   )
